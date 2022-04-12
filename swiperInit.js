72321bf3bce9caa7c4654b89c98614d2d2a75e99
sadbc1235 <sadbc1235@naver.com>
2022-04-13 00:39:17 +0900

    step2 complet swiper-gallery

--- swiperInit.js
+++ swiperInit.js
@@ -3,7 +3,26 @@ const swiper = new Swiper("main", {
   loop: true, // 스와이프를 순환
   direction: "horizontal", // horizontal 가로, vertical 세로 스와이프
   grabCursor: true,
-  slidesPerView: 3, // 숫자(해당 숫자 단위로 패널 표시)
+  slidesPerView: "auto", // 숫자(해당 숫자 단위로 패널 표시)
   spaceBetween: 20, // 패널 사이 간격 (px)
   centeredSlides: true, // 활성화 패널을 항상 가로 가운대 배치
+
+  pagination: {
+    el: ".swiper-pagination", // 페이지네이션 버튼 추가
+    // clickable: true, // 클릭 가능 여부 설정
+    type: "fraction",
+  },
+
+  navigation: {
+    prevEl: ".swiper-button-prev",
+    nextEl: ".swiper-button-next",
+  },
+
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 50, // y축 회전각도
+    stretch: -100, // 당겨짐 정도
+    depth: 400, // 입체적인 왜곡효과 정도
+    slideShadows: false, // 패널의 그림자 설정
+  },
 });
